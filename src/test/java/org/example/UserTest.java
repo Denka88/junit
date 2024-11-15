175326655f0b12ed8cc98eab8426bc4df6c87b3a
Denka88 <shadowfightdenka88@gmail.com>
2024-11-15 14:01:51 +0300

    Продолжить работу

---
+++ src/test/java/org/example/UserTest.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class UserTest {
+
+    @Test
+    public void getAllUsers() {
+
+        User user1 = new User("Евгений", 35, Sex.MALE);
+        User user2 = new User("Марина", 34, Sex.FEMALE);
+        User user3 = new User("Алина", 7, Sex.FEMALE);
+
+    }
+
+    @Test
+    public void testGetAllUsers() {
+    }
+
+    @Test
+    public void getHowManyUsers() {
+    }
+
+    @Test
+    public void testGetHowManyUsers() {
+    }
+
+    @Test
+    public void getAllAgeUsers() {
+    }
+
+    @Test
+    public void testGetAllAgeUsers() {
+    }
+
+    @Test
+    public void getAverageAgeOfAllUsers() {
+    }
+
+    @Test
+    public void testGetAverageAgeOfAllUsers() {
+    }
+}
